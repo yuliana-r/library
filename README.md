e06f890b6d08a55e9f91920f36a8601ce2a2ae71
yuli-r <iuliana.rotaru234@gmail.com>
2022-02-17 19:48:21 +0000

    Edit README.md, create 5 books

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # library
+
+Tab icon source - <a href="https://www.flaticon.com/">Flaticon</a>
---
+++ script.js
@@ -0,0 +1,37 @@
+let myLibrary = [book1, book2, book3, book4, book5];
+
+
+function Book(title, author, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+}
+
+Book.prototype.info = function () {
+    return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.status;
+}
+
+function addBookToLibrary() {
+
+}
+
+myLibrary.fill(book1, book2, book3);
+
+myLibrary.forEach(book => {
+    for (let key in book) {
+        const e = document.createElement('p');
+        e.textContent = `${book[key]}`;
+        document.body.appendChild(e);
+    }
+})
+
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
+const book2 = new Book("Harry Potter and the Goblet of Fire", "J. K. Rowling",
+    "636", "read");
+const book3 = new Book("Pride and Prejudice", "Jane Austen",
+    "279", "read");
+const book4 = new Book("Me Before You", "Jojo Moyes",
+    "369", "read");
+const book5 = new Book("Educated", "Tara Westover",
+    "334", "read");
